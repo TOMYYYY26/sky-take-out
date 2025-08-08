c745ee0588c6e1ef389bc49fa89a6362ece5f8da
Ye Rui <yrrrr1226@qq.com>
2025-08-08 17:19:54 +0800

    // aliyun oss 配置类

---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,38 @@
+package com.sky.controller.admin;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.sky.result.Result;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 通用接口
+ */
+@RestController
+@RequestMapping("/admin/common")
+@Api(tags = "通用接口")
+@Slf4j
+public class CommonController {
+
+    /**
+     * 上传文件
+     * @param file
+     * @return
+     */
+    @PostMapping("/upload")
+    @ApiOperation("上传文件")
+    public Result<String> upload(MultipartFile file) {
+        log.info("上传文件:{}",file);
+
+
+        
+        return Result.success();
+    }
+    
+}
